43c9331b41db04d1bec35da09afab52344e5ad38
zy <zy@zys-MacBook-Pro.local>
2025-07-24 00:12:07 +0800

    chore: ios config update

--- app.config.ts
+++ app.config.ts
@@ -43,7 +43,8 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
           deploymentTarget: '15.1'
         }
       }
-    ]
+    ],
+    'react-native-maps'
   ],
   updates: {
     fallbackToCacheTimeout: 0
