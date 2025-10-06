82107e3072209d90341a246a12f3cf8f2ec4ca7c
Zhen Yi Ang <angzhenyi@gmail.com>
2025-10-07 01:22:07 +0800

    chore: revert metro changes

--- metro.config.js
+++ metro.config.js
@@ -3,18 +3,4 @@ const { getDefaultConfig } = require('expo/metro-config')
 
 const config = getDefaultConfig(__dirname)
 
-// Bundle optimization
-config.transformer.minifierConfig = {
-  keep_fnames: false,
-  mangle: {
-    keep_fnames: false
-  }
-}
-
-// Remove unused code
-config.transformer.unstable_allowRequireContext = false
-
-// Optimize bundle size
-config.resolver.platforms = ['ios', 'android', 'native', 'web']
-
 module.exports = config
